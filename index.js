951bc81eb4dd04914a2df53145cd246b166333ab
Jessica Kerr <jessitron@gmail.com>
2024-11-29 14:45:37 -0600

    Abstraction. Make it easier to report a span when a click happens

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 console.log("ren was here lol");
 
-const inSpan = window.Hny.inSpan; // this is added by my hny-otel-web script, which is loaded in the HTML.
+// just in case my hny-otel-web script didn't load, make it so this won't fail the whole page
+const inSpan = window.Hny?.inSpan || ((serviceName, spanName, fn) => fn());
+
+// construct a function that works as an onClick event, while reporting to Honeycomb
+function reportClick(description, onClickFn) {
+  return (event) => {
+    inSpan("health-website", description, () => {
+      onClickFn(event);
+    });
+  };
+}
 
 const brain = document.getElementById("brain-area");
-brain.onclick =  () => inSpan("health-website", "click brain",() => {
+brain.onclick = reportClick("click brain", () => {
   const box_to_open = document.getElementById("brain-box");
-  box_to_open.style.display="block";
+  box_to_open.style.display = "block";
 });
 
 const x_button = document.getElementById("close-box-button");
-x_button.onclick = (event) => inSpan("health-website", "close brain box", () => {
-    const box_to_close = event.target.closest(".organ-box");
-    box_to_close.style.display="none";
+x_button.onclick = reportClick("close brain box", (event) => {
+  const box_to_close = event.target.closest(".organ-box");
+  box_to_close.style.display = "none";
 });
